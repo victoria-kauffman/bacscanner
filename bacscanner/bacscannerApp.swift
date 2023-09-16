66d34138d77ebfa697eaa26c4343d01fe3b189d6
Victoria Kauffman <victoriakauffman@college.harvard.edu>
2023-09-16 01:43:23 -0400

    Initial Commit

---
+++ bacscanner/bacscannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bacscannerApp.swift
+//  bacscanner
+//
+//  Created by Victoria Kauffman on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct bacscannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bacscannerUITests/bacscannerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bacscannerUITestsLaunchTests.swift
+//  bacscannerUITests
+//
+//  Created by Victoria Kauffman on 9/16/23.
+//
+
+import XCTest
+
+final class bacscannerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

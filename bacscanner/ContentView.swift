a1bd388d404f226a8d17456ef6137443150d3f1a
Victoria Kauffman <victoriakauffman@college.harvard.edu>
2023-09-16 13:50:35 -0400

    Start of opening page

--- bacscanner/ContentView.swift
+++ bacscanner/ContentView.swift
@@ -10,34 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showEstimateEyes = false
-    @State private var bac = false
     @State private var showSoberPicAlert = false
+    
     @State var name: String = UserDefaults.standard.string(forKey: "USER_NAME") ?? ""
-    @State var inputText: String = ""
-    private var displayedBac: String {
-        return "You shouldn't be on the road, you fucking drunkard"
-    }
+    @State var inputName: String = ""
+    
+    @State var soberPicData = UserDefaults.standard.data(forKey: "SOBER_DATA") ?? nil
+    @State var soberInput: UIImage? = nil
 
     var body: some View {
-        Grid(alignment: .bottom, horizontalSpacing: 10, verticalSpacing: 10) {
-                GridRow {
-                    Text("Tips")
-                        .bold()
-                        .background(.cyan)
-                    Button("Estimate BAC") {
-                        showEstimateEyes.toggle()
-                    }
-                    .background(.pink)
-                    .sheet(isPresented: $showEstimateEyes) {
-                        CameraView().ignoresSafeArea()
-                    }
-                }
-                GridRow {
-                    Text(displayedBac).bold()
-                        .background(.orange)
-                    Text("Chatbot").bold()
-                        .background(.red)
-                }
+        Form {
+            Section(header: Text("Name")) {
+                TextField("Enter First Name", text: $inputName)
+            }
+            Section(header: Text("Sober Picture")) {
                 Button("Take Sober Photo") {
                     showSoberPicAlert.toggle()
                 }
@@ -57,47 +43,32 @@ struct ContentView: View {
                         )
                     )
                 }
-                .bold()
-                .background(.yellow)
                 .sheet(isPresented: $showEstimateEyes) {
                     CameraView().ignoresSafeArea()
                 }
+            }
+            Section(header: Text("Actions:")) {
+                Button("Save Data") {
+                    // Save username
+                    UserDefaults.standard.set(inputName, forKey: "USER_NAME")
+                    name = inputName
+                    print("Saved name: \(name)")
+                    /*
+                    // Save sober image
+                    guard let data = soberInput.jpegData(compressionQuality: 0.5) else { return }
+                        let encoded = try! PropertyListEncoder().encode(data)
+                        UserDefaults.standard.set(encoded, forKey: "KEY")
+                    
+                
+                     */
+                }
+            }
+            Section(header: Text("Saved Data:")) {
+                Text(name)
+            }
         }
     }
 }
-
-/*
-let dialogMessage = "For best results, you should take this photo completely sober. Are you sure you should continue?"
-let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: .alert)
-alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-    switch action.style{
-        case .default:
-        print("default")
-        
-        case .cancel:
-        print("cancel")
-        
-        case .destructive:
-        print("destructive")
-        
-    }
-}))
-self.present(alert, animated: true, completion: nil)
- */
-
-/*.sheet(isPresented: $showEstimateEyes) {
-    Form {
-        Section("Settings") {
-            Toggle("Drunk?", isOn: $bac)
-        }
-        Button("Done") {
-            showEstimateEyes.toggle()
-        }
-        Section("Preview") {
-            Text("Meep")
-        }
-    }
-} */
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
